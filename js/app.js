a60007d1da55264441b66a93bca662ba5ed48443
Alfer Brent Mercado <alfzmercado240@gmail.com>
2025-11-23 18:34:02 +0800

    animations

--- js/app.js
+++ js/app.js
@@ -21,4 +21,19 @@ addEventListener("DOMContentLoaded", () => {
   }
 
   animateCursor();
+
+  // aos
+
+  window.addEventListener("load", () => {
+    AOS.init({
+      duration: 1000,
+      delay: 100,
+      once: false,
+      offset: 100,
+    });
+
+    setTimeout(() => {
+      AOS.refresh();
+    }, 500);
+  });
 });
