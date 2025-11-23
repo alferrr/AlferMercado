371f46999650f8b470a624b14278f6c2f08d33dd
Alfer Brent Mercado <alfzmercado240@gmail.com>
2025-11-23 16:10:14 +0800

    first commit

---
+++ js/app.js
@@ -0,0 +1,24 @@
+addEventListener("DOMContentLoaded", () => {
+  const cursor = document.querySelector(".cursor");
+  let mouseX = 0;
+  let mouseY = 0;
+  let cursorX = 0;
+  let cursorY = 0;
+
+  document.addEventListener("mousemove", (e) => {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+  });
+
+  function animateCursor() {
+    cursorX += (mouseX - cursorX) * 0.2;
+    cursorY += (mouseY - cursorY) * 0.2;
+
+    cursor.style.left = cursorX + "px";
+    cursor.style.top = cursorY + "px";
+
+    requestAnimationFrame(animateCursor);
+  }
+
+  animateCursor();
+});
